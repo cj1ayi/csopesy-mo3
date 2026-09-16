ba578e8b553371ad1624dc2e867d474ac7bb3391
Joshua Nacasabog <joshua.nacasabog@gmail.com>
2026-09-16 11:49:28 +0800

    refactor: use std:: prefix instead of using namespace

--- SOURCE/helpers.cpp
+++ SOURCE/helpers.cpp
@@ -2,27 +2,26 @@
 #include <iostream>
 #include <algorithm>
 #include <cctype>
-using namespace std;
 
 /**
  * Prints the list of available commands.
  */
 void printHelp()
 {
-    cout    << "Available commands:\n"
-            << "  help          - show list of available commands\n"
-            << "  start_marquee - start the animation\n"
-            << "  stop_marquee  - stop the animation\n"
-            << "  set_text      - set marquee text\n"
-            << "  set_speed     - set refresh speed (ms)\n"
-            << "  exit          - quit the program\n";
+    std::cout << "Available commands:\n"
+              << "  help          - show list of available commands\n"
+              << "  start_marquee - start the animation\n"
+              << "  stop_marquee  - stop the animation\n"
+              << "  set_text      - set marquee text\n"
+              << "  set_speed     - set refresh speed (ms)\n"
+              << "  exit          - quit the program\n";
 }
 
 /**
  * Converts a string to lowercase
  */
-string toLower(string s)
+std::string toLower(std::string s)
 {
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
     return s;
 }
--- SOURCE/main.cpp
+++ SOURCE/main.cpp
@@ -1,41 +1,40 @@
 #include <iostream>
 #include "helpers.h"
-using namespace std;
 
 int main()
 {
-    string command;
+    std::string command;
 
     while (true)
     {
-        cout << ">";
-        getline(cin, command);
+        std::cout << ">";
+        getline(std::cin, command);
 
         command = toLower(command);
 
-        if(command == "help")
+        if (command == "help")
         {
             printHelp();
         }
         else if (command == "start_marquee")
         {
-            //TODO
-            cout << "TODO: start_marquee\n";
+            // TODO
+            std::cout << "TODO: start_marquee\n";
         }
         else if (command == "stop_marquee")
         {
-            //TODO
-            cout << "TODO: stop_marquee\n";
+            // TODO
+            std::cout << "TODO: stop_marquee\n";
         }
         else if (command == "set_text")
         {
-            //TODO
-            cout << "TODO: set_text\n";
+            // TODO
+            std::cout << "TODO: set_text\n";
         }
         else if (command == "set_speed")
         {
-            //TODO
-            cout << "TODO: set_speed\n";
+            // TODO
+            std::cout << "TODO: set_speed\n";
         }
         else if (command == "exit")
         {
@@ -43,7 +42,7 @@ int main()
         }
         else
         {
-            cout << "Unknown command. Type HELP to view the list of available commands.\n";
+            std::cout << "Unknown command. Type HELP to view the list of available commands.\n";
         }
     }
 
